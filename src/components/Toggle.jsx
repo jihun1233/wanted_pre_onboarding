8f581cf9f09e80d9a33ac8470b4b402ca38d62fd
jihun1233 <leejihun1233@gmail.com>
2022-04-15 23:39:39 +0900

    Toggle 컴포넌트 추가
    - checkbox를 사용
    
    - state를 변경하여 styled-components의 props로 넘겨주어 translateX로 이동시켜 체크된 상황을 표시

--- src/components/Toggle.jsx
+++ src/components/Toggle.jsx
@@ -1,7 +1,65 @@
-import React from "react";
-
+import React, { useState } from "react";
+import styled from "styled-components";
+const ToggleBox = styled.div`
+  position: relative;
+  margin: 0;
+  width: 400px;
+  height: 50px;
+`;
+const Input = styled.input`
+  margin: 0;
+  padding: 0;
+  position: relative;
+  appearance: none;
+  width: 400px;
+  height: 50px;
+  background-color: #f1f1f1;
+  border-radius: 60px;
+  &:before {
+    top: 5px;
+    left: 10px;
+    position: absolute;
+    box-sizing: border-box;
+    border-radius: 60px;
+    content: "";
+    background: white;
+    width: 180px;
+    height: 40px;
+    transition: 0.2s;
+  }
+  &:checked {
+    &::before {
+      left: 210px;
+    }
+  }
+`;
+const TextBox = styled.div`
+  pointer-events: none;
+  position: absolute;
+  margin: 0;
+  padding: 0;
+  top: 0;
+  width: 400px;
+  height: 50px;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+`;
 function Toggle() {
-  return <div></div>;
+  const [toggle, setToggle] = useState(false);
+  const onToggle = () => {
+    console.log(toggle);
+    setToggle(!toggle);
+  };
+  return (
+    <ToggleBox>
+      <Input type="checkbox" value={toggle} onChange={onToggle}></Input>
+      <TextBox>
+        <span>기본</span>
+        <span>상세</span>
+      </TextBox>
+    </ToggleBox>
+  );
 }
 
 export default Toggle;
